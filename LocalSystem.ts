eb4309cebc5dce7cc3dd9d0a78fc3f23a8e23b3c
alextoti <alexandrostitonis@gmail.com>
2022-03-09 13:19:14 +0200

    fixed issues with the chat

--- LocalSystem.ts
+++ LocalSystem.ts
@@ -1,45 +1,56 @@
-import { isClient } from '@xrengine/engine/src/common/functions/isClient'
-import { World } from '@xrengine/engine/src/ecs/classes/World'
 import { pipe } from 'bitecs'
+
 import { client } from '@xrengine/client-core/src/feathers'
-import { handleCommand, isCommand } from './commandHandler'
-import { accessAuthState } from '@xrengine/client-core/src/user/services/AuthService'
-import { isPlayerLocal } from '@xrengine/engine/src/networking/utils/isPlayerLocal'
-import { Engine } from '@xrengine/engine/src/ecs/classes/Engine'
 import {
   getChatMessageSystem,
   hasSubscribedToChatSystem,
   removeMessageSystem
 } from '@xrengine/client-core/src/social/services/utils/chatSystem'
+import { accessAuthState } from '@xrengine/client-core/src/user/services/AuthService'
 import { isBot } from '@xrengine/engine/src/common/functions/isBot'
+import { isClient } from '@xrengine/engine/src/common/functions/isClient'
+import { Engine } from '@xrengine/engine/src/ecs/classes/Engine'
+import { World } from '@xrengine/engine/src/ecs/classes/World'
+import { isPlayerLocal } from '@xrengine/engine/src/networking/utils/isPlayerLocal'
+
+import { handleCommand, isCommand } from './commandHandler'
 
 console.log('loaded local systems')
 
 console.log('loaded chat system')
 client.service('message').on('created', (params) => {
-    const selfUser = accessAuthState().user.value
-    const { message } = params
-    console.log('new message: ' + message)
+  const selfUser = accessAuthState().user.value
+  const { message } = params
+  console.log(
+    'BOT_MESSAGE|',
+    JSON.stringify({
+      id: message.id,
+      sender: message.sender.name,
+      senderId: message.sender.userId,
+      channelId: message.channelId,
+      text: message.text,
+      updatedAt: message.updatedAt
+    })
+  )
 
-    if (message != undefined && message.text != undefined) {
-      if (isPlayerLocal(message.senderId)) {
-        if (handleCommand(message.text, Engine.currentWorld.localClientEntity, message.senderId)) return
-        else {
-          const system = getChatMessageSystem(message.text)
-          if (system !== 'none') {
-            message.text = removeMessageSystem(message.text)
-            if (!isBot(window) && !Engine.isBot && !hasSubscribedToChatSystem(selfUser.id, system)) return
-          }
-        }
-      } else {
+  if (message != undefined && message.text != undefined) {
+    if (isPlayerLocal(message.senderId)) {
+      if (handleCommand(message.text, Engine.currentWorld.localClientEntity, message.senderId)) return
+      else {
         const system = getChatMessageSystem(message.text)
         if (system !== 'none') {
           message.text = removeMessageSystem(message.text)
-          if (!isBot(window) && !Engine.isBot && !Engine.isBot && !hasSubscribedToChatSystem(selfUser.id, system))
-            return
-        } else if (isCommand(message.text) && !Engine.isBot && !isBot(window)) return
+          if (!isBot(window) && !Engine.isBot && !hasSubscribedToChatSystem(selfUser.id, system)) return
+        }
       }
+    } else {
+      const system = getChatMessageSystem(message.text)
+      if (system !== 'none') {
+        message.text = removeMessageSystem(message.text)
+        if (!isBot(window) && !Engine.isBot && !Engine.isBot && !hasSubscribedToChatSystem(selfUser.id, system)) return
+      } else if (isCommand(message.text) && !Engine.isBot && !isBot(window)) return
     }
+  }
 })
 
 export default async function LocalSystem(world: World) {
